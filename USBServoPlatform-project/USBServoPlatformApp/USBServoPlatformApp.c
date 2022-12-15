fdfa664141986db1252f43efc829d03effac4219
theimposingdwarf <67246298+theimposingdwarf@users.noreply.github.com>
2022-12-14 22:13:17 -0700

    Added multicore and blink from core1

--- USBServoPlatform-project/USBServoPlatformApp/USBServoPlatformApp.c
+++ USBServoPlatform-project/USBServoPlatformApp/USBServoPlatformApp.c
@@ -1,17 +1,21 @@
 #include "pico/stdlib.h"
+#include "pico/multicore.h"
 
-int main() {
-#ifndef PICO_DEFAULT_LED_PIN
-#warning blink example requires a board with a regular LED
-#else
+void core1_main(){
     const uint LED_PIN = PICO_DEFAULT_LED_PIN;
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
     while (true) {
         gpio_put(LED_PIN, 1);
-        sleep_ms(50);
+        sleep_ms(250);
         gpio_put(LED_PIN, 0);
-        sleep_ms(50);
+        sleep_ms(250);
     }
-#endif
+}
+
+int main(){
+    stdio_init_all();
+    //Launch Second Core
+    multicore_launch_core1(core1_main);
+    while(1);
 }
